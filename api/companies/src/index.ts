31de8703fbdcbc94a8dc5e5ff897bdccf4a565d3
Cristian Berrio <cristianberrio9864@gmail.com>
2023-06-09 04:59:55 -0500

    front end

--- api/companies/src/index.ts
+++
@@ -1,8 +0,0 @@
-import app from './app';
-import env from './dotenv';
-
-const port = env.port;
-
-app.listen(port, () => {
-  console.log(`Servidor escuchando en el puerto ${port}`);
-});
--- api/inventory/src/dotenv.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export default {
-  port: process.env.PORT || "",
-  mongoURI: process.env.MONGODB_URI || "",
-  jwtSecret: process.env.JWT_SECRET || "",
-};
